06da1b95b531d9fc2b5874fe440be51c980a706c
S <2438019@cognizant.com>
2025-10-07 22:15:19 +0530

    My updates to testdata, BasePage, and utility files

--- Display_Bookshelves/utils/test-base.ts
+++ Display_Bookshelves/utils/test-base.ts
@@ -1,17 +1,17 @@
 import { test as baseTest } from '@playwright/test';
 import { readCSVData } from './readCSV';
-
+ 
 type TestData = {
   category: string;
   field: string;
   value: string;
 };
-
+ 
 export const test = baseTest.extend<{
   testData: TestData[];
 }>({
   testData: async ({}, use) => {
-    const data = readCSVData('utils/data.csv') as TestData[];
+    const data = readCSVData() as TestData[];
     await use(data);
   }
 });
